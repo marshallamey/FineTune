280d8d20c6ae22d828e947f8a2aebfcf86f4dc11
Marshall Amey <marshall@marshallamey.com>
2019-12-20 19:54:47 -0800

    remove server

--- server/config/keys.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  // IN PRODUCTION! return production keys
-  module.exports = require('./prod');
-} else {
-  // IN DEVELOPMENT! return dev keys
-  module.exports = require('./dev');
-}
-/* eslint-enable */
--- server/config/prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
-  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  spotifyRedirectURI: process.env.SPOTIFY_REDIRECT_URI,
-  spotifyCallbackURI: process.env.SPOTIFY_CALLBACK_URI,
-  devURI: process.env.DEV_URI,
-  prodURI: process.env.PROD_URI,
-};
