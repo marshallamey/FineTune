9a10f91122a402cbd209509e5aef309797a64295
MJAdmin <mjadmin@MacBook-Pro.local>
2020-04-30 13:16:21 -0700

    LoginButton fixed.

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function LoginButton() {
     return (
-        <a href={`https://finetune.io/spotify/logout`}>
+        <a href={`https://finetune.io/spotify/login`}>
             <button className="btn__login">
                 <div className='logo'>
                     <img className="spotify-logo" src="/img/spotify-logo.png" alt="spotify-logo.JPG" />
