98b55397cc20d3d017dba2baaf6a59e50dad6ad7
Marshall Amey <marshallamey@mj-laptop.mjadmin.dom>
2020-11-23 13:16:57 -0800

    code refactor to specs

---
+++ routes/spotify.js
@@ -0,0 +1,94 @@
+// routes/spotify.js
+const express = require('express');
+const router = express.Router();
+
+// AUTH ROUTES
+ 
+/* FineTune sends request for authorization to Spotify */
+router.get('/spotify/login', (req, res) => {   
+  const scope = 'user-read-private playlist-modify-public user-library-modify user-modify-playback-state user-read-playback-state user-read-currently-playing';
+  const stateValue = generateRandomString(16);
+  res.cookie(stateKey, stateValue);  
+  res.redirect(`https://accounts.spotify.com/authorize?${
+      querystring.stringify({
+      response_type: 'code',
+      show_dialog: false,
+      client_id: vars.SPOTIFY_CLIENT_ID,
+      scope,
+      redirect_uri: vars.SPOTIFY_REDIRECT_URI,
+      state: stateValue,
+  })}`);
+});
+
+/* FineTune removes authorization to Spotify */
+router.get('/spotify/logout', (req, res) => {
+  res.clearCookie(stateKey);
+  res.redirect('http://finetune.io/');
+});
+
+/* Finetune requests refresh and access tokens after checking the state parameter */
+router.get('/spotify/callback', (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+      res.redirect(`/#${
+          querystring.stringify({
+              error: 'state_mismatch',
+      })}`);
+  } else {
+      res.clearCookie(stateKey);
+      const authOptions = {
+          url: 'https://accounts.spotify.com/api/token',
+          form: {
+              code,
+              redirect_uri: vars.SPOTIFY_REDIRECT_URI,
+              grant_type: 'authorization_code',
+          },
+          headers: {
+              Authorization: `Basic ${(Buffer.from(`${vars.SPOTIFY_CLIENT_ID}:${vars.SPOTIFY_CLIENT_SECRET}`).toString('base64'))}`,
+          },
+          json: true,
+      };
+
+      request.post(authOptions, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+              const { access_token, refresh_token } = body;
+              // we can also pass the token to the browser to make requests from there
+              res.redirect(vars.SPOTIFY_CALLBACK_URI
+                  + querystring.stringify({
+                  access_token,
+                  refresh_token,
+                  }));
+          } else {
+              res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+          }
+      });
+  }
+});
+
+/* FineTune requests access token from Spotify using refresh token */
+router.get('/spotify/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+  const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { Authorization: `Basic ${(Buffer.from(`${vars.SPOTIFY_CLIENT_ID}:${vars.SPOTIFY_CLIENT_SECRET}`).toString('base64'))}` },
+      form: {
+      grant_type: 'refresh_token',
+      refresh_token,
+      },
+      json: true,
+  };
+
+  request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+          const { access_token } = body;
+          res.send({
+              access_token,
+          });
+      }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**
  * This is a node.js script that performs the Authorization Code oAuth2
- *  flow to authenticate against multiple accounts.
+ *  flow to authenticate against Spotify accounts.
  *
  * For more information about Spotify Auth Flow, read the following:
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
@@ -8,45 +8,34 @@
 
 /* eslint-disable camelcase */
 const cookieParser = require('cookie-parser');
-const querystring = require('querystring');
-const request = require('request');
+const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path')
 const cors = require('cors');
 const AWS = require('aws-sdk');
+
 const stateKey = 'spotify_auth_state';
+const PORT = process.env.NODE_PORT || 8081;
 
-const app = express();
-console.log('AUTH SERVER STARTED...');
+const server = express();
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true, }))
 
-/** Get environment variables from AWS Parameter Store **/
-// if (process.env.NODE_ENV !== 'prod') {
-//     var myCredentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-//     AWS.config.credentials = myCredentials;
-// }
-
+/* GET ENVIRONMENT VARIABLES */
 AWS.config.region = 'us-west-2';
-
-
 const ssm = new AWS.SSM();
-let keys = {
+const vars = {
     SPOTIFY_CLIENT_ID: '',
     SPOTIFY_CLIENT_SECRET: '',
     SPOTIFY_REDIRECT_URI: '',
     SPOTIFY_CALLBACK_URI: '',
-    WEBSITE_KEY: '',
 };
-for (const key in keys) {
-    let value = ssm.getParameter({ Name: key, WithDecryption: true }).promise();
-    value.then(res => { 
-        console.log("HEY!" + res);
-        keys[key] = res.Parameter.Value; 
-    })
-    .catch( err => { console.log(err); })
+for (const v in vars) {
+    let value = ssm.getParameter({ Name: `/finetune.io/${v}`, WithDecryption: true }).promise()
+    value.then(res => vars[v] = res.Parameter.Value)
+    .catch(err => console.log("ERROR retrieving env variables from AWS:", err))
 }
-ssm.getParameter({ Name: "/MJAdmin/WEBSITE_KEY", WithDecryption: true }).promise()
-    .then(res => { keys['WEBSITE_KEY'] = res.Parameter.Value })
-    .catch( err => err)
-    
+
 /** Generate a random string containing numbers and letters **/
 const generateRandomString = (length) => {
     let text = '';
@@ -57,122 +46,25 @@ const generateRandomString = (length) => {
     return text;
 };
 
+/* DETERMINE ROUTES */   
+server.use('/spotify', require('./routes/spotify'))
 
-app.use(express.static(`${__dirname}/public`))
+/* DETERMINE NODE ENVIRONMENT */
+if (process.env.NODE_ENV === 'production') {
+    const root = path.join(__dirname, 'client', 'build')
+    server.use(express.static(root));
+    server.get("*", (req, res) => {
+        res.sendFile('index.html', { root });
+    })
+} else {
+server.use(express.static(`${__dirname}/public`))
     .use(cors())
     .use(cookieParser());
+}
 
-app.get('/', (req,res) => {
-    res.send('Auth Server running...');
-});
-
-/* FineTune removes authorization to Spotify */
-app.get('/marshallamey/login', (req, res) => {
-    console.log(req.query.key)
-    const key = req.query.key || null;
-    if (key === keys.WEBSITE_KEY) res.redirect('http://marshallamey.com/dashboard')
-    else res.redirect('http://marshallamey.com/login');
-});
-
-/* FineTune sends request for authorization to Spotify */
-app.get('/spotify/login', (req, res) => {   
-    const scope = 'user-read-private playlist-modify-public user-library-modify user-modify-playback-state user-read-playback-state user-read-currently-playing';
-    const stateValue = generateRandomString(16);
-    res.cookie(stateKey, stateValue);  
-    res.redirect(`https://accounts.spotify.com/authorize?${
-        querystring.stringify({
-        response_type: 'code',
-        show_dialog: false,
-        client_id: keys.SPOTIFY_CLIENT_ID,
-        scope,
-        redirect_uri: keys.SPOTIFY_REDIRECT_URI,
-        state: stateValue,
-    })}`);
-});
-
-/* FineTune removes authorization to Spotify */
-app.get('/spotify/logout', (req, res) => {
-    res.clearCookie(stateKey);
-    res.redirect('http://finetune.io/');
-});
-
-/* Finetune requests refresh and access tokens after checking the state parameter */
-app.get('/spotify/callback', (req, res) => {
-    const code = req.query.code || null;
-    const state = req.query.state || null;
-    const storedState = req.cookies ? req.cookies[stateKey] : null;
-
-    if (state === null || state !== storedState) {
-        res.redirect(`/#${
-            querystring.stringify({
-                error: 'state_mismatch',
-        })}`);
-    } else {
-        res.clearCookie(stateKey);
-        const authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code,
-                redirect_uri: keys.SPOTIFY_REDIRECT_URI,
-                grant_type: 'authorization_code',
-            },
-            headers: {
-                Authorization: `Basic ${(Buffer.from(`${keys.SPOTIFY_CLIENT_ID}:${keys.SPOTIFY_CLIENT_SECRET}`).toString('base64'))}`,
-            },
-            json: true,
-        };
-
-        request.post(authOptions, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                const { access_token, refresh_token } = body;
-                // we can also pass the token to the browser to make requests from there
-                res.redirect(keys.SPOTIFY_CALLBACK_URI
-                    + querystring.stringify({
-                    access_token,
-                    refresh_token,
-                    }));
-            } else {
-                res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
-            }
-        });
-    }
-});
-
-/* FineTune requests access token from Spotify using refresh token */
-app.get('/spotify/refresh_token', (req, res) => {
-    const { refresh_token } = req.query;
-    const authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        headers: { Authorization: `Basic ${(Buffer.from(`${keys.SPOTIFY_CLIENT_ID}:${keys.SPOTIFY_CLIENT_SECRET}`).toString('base64'))}` },
-        form: {
-        grant_type: 'refresh_token',
-        refresh_token,
-        },
-        json: true,
-    };
-
-    request.post(authOptions, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const { access_token } = body;
-            res.send({
-                access_token,
-            });
-        }
-    });
-});
-
-// if (process.env.NODE_ENV === 'production') {
-//     // Express will serve up production asset FILES (e.g., main.js, main.css)
-//     app.use(express.static('../client/build'));
-
-//     // OR Express will serve up index.html ROUTES unrecognized
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-//     });
-// }
-
-const PORT = process.env.NODE_PORT || 8081;
-console.log(`FineTune Server started: Listening on ${ PORT }`);
-app.listen(PORT);
+/* LISTEN FOR TRAFFIC */
+server.listen(PORT);
+console.log(`finetune.io server listening on ${ PORT }`);
+console.log(`Node environment set to ${ process.env.NODE_ENV }`);
 
 /* eslint-enable */
